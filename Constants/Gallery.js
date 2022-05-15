b578eb9e7b6440291b35a7f7a6ef2ec930ed3353
Holfz <Holfzcompilation@gmail.com>
2022-05-15 18:43:12 +0700

    fix(Constants): add support for reused and event map

--- Constants/Gallery.js
+++ Constants/Gallery.js
@@ -1,13 +1,25 @@
 module.exports = Object.freeze({
+    // Reused Map
+    "#mp_rr_canyonlands_staging": "firing-range",
+    "#mp_rr_canyonlands_staging_desc": "firing-range",
+
+    // Event Map
+    "#mp_rr_canyonlands_mu1_night": "shadow-royale",
+    "#mp_rr_canyonlands_mu1_night_desc": "shadow-royale",
+    "#mp_rr_desertlands_holiday": "winter-express",
+    "#mp_rr_desertlands_holiday_desc": "winter-express",
+
+    // Normal Map
     "#mp_rr_canyonlands": "kings-canyon",
-    "#mp_rr_desertlands": "winter-express",
+    "#mp_rr_desertlands": "world-edge",
     "#mp_rr_olympus": "olympus",
+    "#mp_rr_tropic_island": "stormpoint",
+
+    // Arena Map
     "#mp_rr_arena_phase_runner": "phase-runner",
     "#mp_rr_party_crasher": "party-crasher",
     "#mp_rr_aqueduct": "overflow",
-    "#mp_rr_tropic_island": "stormpoint",
-    "mp_rr_tropic_island": "stormpoint",
-    "mp_rr_arena_skygarden": "encore",
+    "#mp_rr_arena_skygarden": "encore",
     "#mp_rr_arena_composite": "drop-off",
     "#mp_rr_arena_habitat": "habitat"
 });
--- Constants/Translation.js
+++ Constants/Translation.js
@@ -1,8 +1,13 @@
 module.exports = Object.freeze({
+    // Manual Firing Range
     "#PL_FIRINGRANGE-ALONE": "Shooting Dummy in the Firing Range",
     "#PL_FIRINGRANGE-PARTY": "Shooting Friends in the Firing Range",
+
+    // Lobby
     "#RICHPRESENCE_PARTY_LOBBY_N_N": "Chilling in the Lobby...",
     "#RICHPRESENCE_PARTY_LOBBY": "Chilling in the Lobby...",
+
+    // Game Mode
     "#PL_TRIO": "TRIOS",
     "#PL_DUO": "DUOS",
     "#PL_Ranked_Leagues": "RANKED",
@@ -13,10 +18,21 @@ module.exports = Object.freeze({
     "#PL_TRAINING": "In the training",
     "#LOADINGSCREEN": "Loading into the battle",
     "#MAINMENU": "In the Main Menu",
+
+    // Map Level
+    // Event Map
+    "#mp_rr_canyonlands_mu1_night": "Shadow Royale",
+    "#mp_rr_canyonlands_mu1_night_desc": "Shadow Royale",
+    "#mp_rr_desertlands_holiday": "Winter Express",
+    "#mp_rr_desertlands_holiday_desc": "Winter Express",
+
+    // Normal Map
     "#mp_rr_canyonlands": "Kings Canyon",
-    "#mp_rr_desertlands": "Winter Express",
+    "#mp_rr_desertlands": "World Edge",
     "#mp_rr_olympus": "Olympus",
     "#mp_rr_tropic_island": "Storm Point",
+
+    // Arena Map
     "#mp_rr_arena_phase_runner": "Phase Runner",
     "#mp_rr_party_crasher": "Party Crasher",
     "#mp_rr_aqueduct": "Overflow",
