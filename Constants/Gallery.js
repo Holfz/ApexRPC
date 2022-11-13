651e124f28eef2b3d9f4562449245c7f5e2cf061
Holfz <Holfzcompilation@gmail.com>
2022-11-13 20:19:15 +0700

    feat(Gallery): Add "After Dark", "Broken Moon", "Hammond Labs", "Barometer", "Caustic Treatment"

--- Constants/Gallery.js
+++ Constants/Gallery.js
@@ -8,12 +8,14 @@ module.exports = Object.freeze({
     "#mp_rr_canyonlands_mu1_night_desc": "shadow-royale",
     "#mp_rr_desertlands_holiday": "winter-express",
     "#mp_rr_desertlands_holiday_desc": "winter-express",
+    "#mp_rr_olympus_mu1_night": "olympus-after-dark",
 
     // Normal Map
     "#mp_rr_canyonlands": "kings-canyon",
     "#mp_rr_desertlands": "world-edge",
     "#mp_rr_olympus": "olympus",
     "#mp_rr_tropic_island": "stormpoint",
+    "#mp_rr_divided_moon": "broken-moon",
 
     // Arena Map
     "#mp_rr_arena_phase_runner": "phase-runner",
@@ -21,5 +23,10 @@ module.exports = Object.freeze({
     "#mp_rr_aqueduct": "overflow",
     "#mp_rr_arena_skygarden": "encore",
     "#mp_rr_arena_composite": "drop-off",
-    "#mp_rr_arena_habitat": "habitat"
+    "#mp_rr_arena_habitat": "habitat",
+
+    // Control Map
+    "#control_map_name_lab": "hammond-labs",
+    "#control_map_name_barometer": "barometer",
+    "#control_map_name_caustic": "caustic-treatment",
 });
